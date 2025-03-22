9968286cf6b37887f4d670cf326d0049d44cedeb
Everton C Prado Jr <ecpj.prado@gmail.com>
2025-03-22 10:52:47 -0300

    168. Excel Sheet Column Title

--- problem_168/src/main.rs
+++ problem_168/src/main.rs
@@ -1,17 +1,26 @@
-fn main() {
-    //let ipt = 18279;
-    let ipt = 2147483647;
+// 168. Excel Sheet Column Title: https://leetcode.com/problems/excel-sheet-column-title
 
+fn main() {
+    let ipt = 2147483647;
     println!("{}", convert_to_title(ipt));
 }
 
+const BOUNDARIES: [usize; 6] = [27, 703, 18279, 475255, 12356631, 321272407];
 pub fn convert_to_title(column_number: i32) -> String {
     let mut n = column_number;
-    let mut title: Vec<u8> = Vec::new();
+    let mut s = BOUNDARIES
+        .iter()
+        .position(|&x| (n as usize) < x)
+        .map_or(7, |i| i + 1);
+
+    let mut title: Vec<u8> = vec![0; s];
 
     while n > 0 {
         let rest = (n % 26) as u8;
-        title.insert(0, if rest == 0 { 90 } else { rest + 64 });
+
+        //                           b'Z'        x + b'A' - 1
+        title[s - 1] = if rest == 0 { 90 } else { rest + 64 };
+        s -= 1;
 
         n = (n - 1) / 26;
     }
@@ -27,8 +36,18 @@ pub fn convert_to_title(column_number: i32) -> String {
     s = (27  * 26) + 1 -> 703   -> AAA
     s = (703 * 26) + 1 -> 18279 -> AAAA
 
-    (18279 - 1) / 26 = 703 -> A
-    (703   - 1) / 26 = 27  -> A
-    (27    - 1) / 26 = 1   -> A
-    (1     - 1) / 26 = 0   -> A
+    (18279 - 1) / 26 = 703 % 26 -> A
+    (703   - 1) / 26 = 27  % 26 -> A
+    (27    - 1) / 26 = 1   % 26 -> A
+    (1     - 1) / 26 = 0   % 26 -> A
+*/
+
+/* Number of characters based on n:
+    1         -> 1
+    27        -> 2
+    703       -> 3
+    18279     -> 4
+    475255    -> 5
+    12356631  -> 6
+    321272407 -> 7
 */
