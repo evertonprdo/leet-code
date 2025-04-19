439c20213fb4101944b4394eab697ba4bfae2e81
Everton C Prado Jr <ecpj.prado@gmail.com>
2025-04-19 08:47:26 -0300

    231. Power of Two

--- src/problem_0231.rs
+++ src/problem_0231.rs
@@ -9,6 +9,15 @@ impl Solution {
     }
 }
 
+pub struct OtherSolution {}
+impl OtherSolution {
+    // Time complexity: O(1)
+    // Space complexity: O(1)
+    pub fn is_power_of_two(n: i32) -> bool {
+        n > 0 && n & (n - 1) == 0
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -16,20 +25,28 @@ mod test {
     #[test]
     fn example_01() {
         let x = 1;
-
         assert!(Solution::is_power_of_two(x));
+        assert!(OtherSolution::is_power_of_two(x));
     }
 
     #[test]
     fn example_02() {
         let x = 16;
-
         assert!(Solution::is_power_of_two(x));
+        assert!(OtherSolution::is_power_of_two(x));
     }
 
     #[test]
     fn example_03() {
         let x = 3;
         assert!(!Solution::is_power_of_two(x));
+        assert!(!OtherSolution::is_power_of_two(x));
+    }
+
+    #[test]
+    fn example_04() {
+        let x = i32::MIN;
+        assert!(!Solution::is_power_of_two(x));
+        assert!(!OtherSolution::is_power_of_two(x));
     }
 }
