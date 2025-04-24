8df5bbbcaec3e4662aea79e5c22946ebf44f27a9
Everton C Prado Jr <ecpj.prado@gmail.com>
2025-04-24 07:11:30 -0300

    374. Guess Number Higher or Lower

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,33 @@
-use leetcode::problem_0003::{IdxSolution, IdxSolution2};
+use leetcode::problem_0374::{LoopSolution, Solution};
 use std::{thread, time::Instant};
 
-// Benchmark to compare performance of different character access methods in a sliding window algorithm:
-// - 256.[idx]: Uses a 256-sized Vec with `[idx]` access
-// -  95.get(): Uses a 95-sized Vec with `.get(idx).unwrap()`
-// -  95.[idx]: Uses a 95-sized Vec with direct `[idx]` indexing
-// -  95.get()&[idx]: Uses a 95-sized Vec with a mix of `.get()` and `[idx]`
-// Goal: Evaluate runtime differences between `.get().unwrap()` and direct `[idx]` access on large input
+// while: 164.154800297s
+// loop: 178.203034157s
 fn main() {
-    let input = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ ".repeat(1_000_000);
-    let output = 95;
-
-    let s1 = input.to_string();
     let handle1 = thread::spawn(move || {
         let now = Instant::now();
-        assert_eq!(IdxSolution::length_of_longest_substring(s1), output);
+
+        for i in 1..i32::MAX {
+            let solution = Solution { pick: i };
+            assert_eq!(solution.guess_number(i32::MAX), i);
+        }
+
         let time = now.elapsed();
-        println!("256.[idx]: {:?}", time);
+        println!("while: {:?}", time);
     });
 
-    let s2 = input.to_string();
     let handle2 = thread::spawn(move || {
         let now = Instant::now();
-        assert_eq!(IdxSolution2::length_of_longest_substring_get(s2), output);
-        let time = now.elapsed();
-        println!(" 95.get(): {:?}", time);
-    });
 
-    let s3 = input.to_string();
-    let handle3 = thread::spawn(move || {
-        let now = Instant::now();
-        assert_eq!(IdxSolution2::length_of_longest_substring_idx(s3), output);
-        let time = now.elapsed();
-        println!(" 95.[idx]: {:?}", time);
-    });
+        for i in 1..i32::MAX {
+            let solution = LoopSolution { pick: i };
+            assert_eq!(solution.guess_number(i32::MAX), i);
+        }
 
-    let s2 = input.to_string();
-    let handle4 = thread::spawn(move || {
-        let now = Instant::now();
-        assert_eq!(IdxSolution2::length_of_longest_substring(s2), output);
         let time = now.elapsed();
-        println!(" 95.get()&[idx]: {:?}", time);
+        println!("loop: {:?}", time);
     });
 
     handle1.join().ok();
     handle2.join().ok();
-    handle3.join().ok();
-    handle4.join().ok();
 }
---
+++ src/problem_0374.rs
@@ -0,0 +1,116 @@
+// 374. Guess Number Higher or Lower: https://leetcode.com/problems/guess-number-higher-or-lower
+use std::cmp::Ordering;
+
+pub struct Solution {
+    pub pick: i32,
+}
+impl Solution {
+    // Time Complexity: O(log n)
+    // Space Complexity: O(1)
+    pub fn guess_number(&self, n: i32) -> i32 {
+        let mut low = 1;
+        let mut high = n;
+
+        while low < high {
+            let mid = low + (high - low) / 2;
+            match self.guess(mid) {
+                0 => return mid,
+                1 => low = mid + 1,
+                _ => high = mid,
+            }
+        }
+
+        n
+    }
+
+    /**
+     * Forward declaration of guess API.
+     * @param  num   your guess
+     * @return 	     -1 if num is higher than the picked number
+     *			      1 if num is lower than the picked number
+     *               otherwise return 0
+     */
+    fn guess(&self, num: i32) -> i32 {
+        match num.cmp(&self.pick) {
+            Ordering::Equal => 0,
+            Ordering::Less => 1,
+            Ordering::Greater => -1,
+        }
+    }
+}
+pub struct LoopSolution {
+    pub pick: i32,
+}
+impl LoopSolution {
+    // Time Complexity: O(log n)
+    // Space Complexity: O(1)
+    pub fn guess_number(&self, n: i32) -> i32 {
+        let mut low = 1;
+        let mut high = n;
+
+        loop {
+            let mid = low + (high - low) / 2;
+            match self.guess(mid) {
+                0 => return mid,
+                1 => low = mid + 1,
+                _ => high = mid - 1,
+            }
+        }
+    }
+
+    /**
+     * Forward declaration of guess API.
+     * @param  num   your guess
+     * @return 	     -1 if num is higher than the picked number
+     *			      1 if num is lower than the picked number
+     *               otherwise return 0
+     */
+    fn guess(&self, num: i32) -> i32 {
+        match num.cmp(&self.pick) {
+            Ordering::Equal => 0,
+            Ordering::Less => 1,
+            Ordering::Greater => -1,
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn example_01() {
+        let input = 10;
+        let pick = 6;
+
+        let solution = Solution { pick };
+        assert_eq!(solution.guess_number(input), pick);
+
+        let solution = LoopSolution { pick };
+        assert_eq!(solution.guess_number(input), pick);
+    }
+
+    #[test]
+    fn example_02() {
+        let input = 1;
+        let pick = 1;
+
+        let solution = Solution { pick };
+        assert_eq!(solution.guess_number(input), pick);
+
+        let solution = LoopSolution { pick };
+        assert_eq!(solution.guess_number(input), pick);
+    }
+
+    #[test]
+    fn example_03() {
+        let input = 2;
+        let pick = 1;
+
+        let solution = Solution { pick };
+        assert_eq!(solution.guess_number(input), pick);
+
+        let solution = LoopSolution { pick };
+        assert_eq!(solution.guess_number(input), pick);
+    }
+}
