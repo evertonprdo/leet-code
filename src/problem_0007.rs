fcccc59e6a174bc4eeee0fb4e7281f8164eaf8bf
Everton C Prado Jr <ecpj.prado@gmail.com>
2025-05-06 17:30:50 -0300

    7. Reverse Integer

--- src/problem_0007.rs
+++ src/problem_0007.rs
@@ -44,6 +44,30 @@ impl CleanSolution {
     }
 }
 
+pub struct PreCheckedSolution {}
+impl PreCheckedSolution {
+    // https://leetcode.com/problems/reverse-integer/solutions/5572539/easy-and-simple-c-approach-beats-100-beginner-friendly/
+    pub fn reverse(mut num: i32) -> i32 {
+        let mut res: i32 = 0;
+        while num != 0 {
+            const BASE: i32 = 10;
+
+            let digit = num % BASE;
+
+            // In this problem it is impossible to get an overflow after multiplication
+            // but this ensures that neither multiplication nor addition causes overflow.
+            // res > (Boundary - digit) / BASE
+            if res > (i32::MAX - digit.abs()) / BASE || res < (i32::MIN + digit.abs()) / BASE {
+                return 0;
+            }
+
+            res = res * BASE + digit;
+            num /= BASE;
+        }
+        res
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -55,6 +79,7 @@ mod test {
 
         assert_eq!(Solution::reverse(x), output);
         assert_eq!(CleanSolution::reverse(x), output);
+        assert_eq!(PreCheckedSolution::reverse(x), output);
     }
 
     #[test]
@@ -64,6 +89,7 @@ mod test {
 
         assert_eq!(Solution::reverse(x), output);
         assert_eq!(CleanSolution::reverse(x), output);
+        assert_eq!(PreCheckedSolution::reverse(x), output);
     }
 
     #[test]
@@ -73,5 +99,36 @@ mod test {
 
         assert_eq!(Solution::reverse(x), output);
         assert_eq!(CleanSolution::reverse(x), output);
+        assert_eq!(PreCheckedSolution::reverse(x), output);
+    }
+
+    #[test]
+    fn example_04() {
+        let x = 2147483647;
+        let output = 0;
+
+        assert_eq!(Solution::reverse(x), output);
+        assert_eq!(CleanSolution::reverse(x), output);
+        assert_eq!(PreCheckedSolution::reverse(x), output);
+    }
+
+    #[test]
+    fn example_05() {
+        let x = 1463847412;
+        let output = 2147483641;
+
+        assert_eq!(Solution::reverse(x), output);
+        assert_eq!(CleanSolution::reverse(x), output);
+        assert_eq!(PreCheckedSolution::reverse(x), output);
+    }
+
+    #[test]
+    fn example_06() {
+        let x = -1463847412;
+        let output = -2147483641;
+
+        assert_eq!(Solution::reverse(x), output);
+        assert_eq!(CleanSolution::reverse(x), output);
+        assert_eq!(PreCheckedSolution::reverse(x), output);
     }
 }
