1976a2259ce30039a1f26eb424d139ee3b95eb50
Everton C Prado Jr <ecpj.prado@gmail.com>
2025-05-30 08:11:10 -0300

    175. Combine Two Tables

---
+++ sql/problem_0175.sql
@@ -0,0 +1,8 @@
+-- 175. Combine Two Tables: https://leetcode.com/problems/combine-two-tables
+
+SELECT 
+    p.firstName, p.lastName, a.city, a.state
+
+FROM Person AS p 
+LEFT JOIN Address AS a 
+    ON p.personId = a.personId;
