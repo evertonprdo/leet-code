3ecd0d7615e4b3d6e5b996ed88537da4107b9c48
Everton C Prado Jr <ecpj.prado@gmail.com>
2025-06-14 09:00:38 -0300

    2798. Number of Employees Who Met the Target

--- src/problem_2798.rs
+++ src/problem_2798.rs
@@ -2,8 +2,13 @@
 
 pub struct Solution;
 impl Solution {
+    // Time complexity: O(n)
+    // Space complexity: O(1)
     pub fn number_of_employees_who_met_target(hours: Vec<i32>, target: i32) -> i32 {
-        hours.into_iter().filter(|&x| x >= target).count() as i32
+        let iter = hours.into_iter();
+        let employees_who_met_target = iter.filter(|&x| x >= target);
+
+        employees_who_met_target.count() as i32
     }
 }
 
